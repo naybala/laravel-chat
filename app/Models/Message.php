af4925bc2bcfbf48f1e1064c0fd02ddc66bb3d12
naybala <naybala.zotefamily@gmail.com>
2023-11-07 13:56:35 +0630

    testing

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['message'];
+
+    /**
+     * A message belong to a user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- config/broadcasting.php
+++ config/broadcasting.php
@@ -13,7 +13,7 @@ return [
     |
     | Supported: "pusher", "ably", "redis", "log", "null"
     |
-     */
+    */
 
     'default' => env('BROADCAST_DRIVER', 'null'),
 
@@ -26,7 +26,7 @@ return [
     | to broadcast events to other systems or over websockets. Samples of
     | each available type of connection are provided inside this array.
     |
-     */
+    */
 
     'connections' => [
 
@@ -36,17 +36,8 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => 'ap1',
-                'useTLS' => true,
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-            ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'useTLS' => true,
             ],
         ],
 
